cd9245fa2153dfe9e52bba833b2f96c7d92d8858
Jadiscke <vjtasso@gmail.com>
2020-08-26 19:01:49 -0300

    Create moving Label

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 const App = () => {
+    const [initialValue,setInitialValue] = useState<number | null>(null);
     return(
         <div className="container">
             <h1>JuroComposto.net</h1>
             <div className="calculator">
                 <div className="input">
-                    <label htmlFor="initial_value">Aporte Inicial</label>
-                    <input type="number" id="initial_value" className="initial_value"/>
+                    {initialValue? 
+                        <label className="valued" htmlFor="initial_value">Aporte Inicial</label>
+                        :<label htmlFor="initial_value">Aporte Inicial</label>
+                    } 
+                    
+                    <input type="number" id="initial_value"  value={initialValue? initialValue : ''}   onChange={(event)=>{setInitialValue(Number(event.target.value))}} className="initial_value"/>
                 </div>
+
+
             </div>
         </div>
-    )
+    );
 }
 
 export default App;
