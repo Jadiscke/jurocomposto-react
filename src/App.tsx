f804cb8a84f487414d40984648899b45d88fd0a2
Jadiscke <vjtasso@gmail.com>
2020-08-26 18:02:52 -0300

    Create Calculator Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import './App.css';
 const App = () => {
     return(
-        <div>
-            <h1>Hello World</h1>
+        <div className="container">
+            <h1>JuroComposto.net</h1>
+            <div className="calculator">
+                <div className="input">
+                    <label htmlFor="initial_value">Aporte Inicial</label>
+                    <input type="number" id="initial_value" className="initial_value"/>
+                </div>
+            </div>
         </div>
     )
 }
